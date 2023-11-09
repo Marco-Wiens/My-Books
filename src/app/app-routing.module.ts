a0715ecfff98e66ebdbc166c59688c50505648ca
Marco-Wiens <marco.wiens@gmail.com>
2023-11-09 14:58:33 +0100

    MyBooks- Retos Lunes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FormRegisterComponent } from './components/form-register/form-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'register',
+    component: FormRegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
