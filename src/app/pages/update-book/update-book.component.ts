e6550a725c6fd57ce6923d08f2ce2da27c450d8d
Marco-Wiens <marco.wiens@gmail.com>
2023-11-14 13:17:46 +0100

    .

--- src/app/pages/update-book/update-book.component.ts
+++ src/app/pages/update-book/update-book.component.ts
@@ -19,14 +19,12 @@ export class UpdateBookComponent {
       // Verifica si ya existe un libro con el mismo id_book
       const existingBook = this.booksService.getAll().find(book => book.id_book === Number(id_book));
 
-      if (existingBook) {
-        alert("Ya existe un libro con el mismo ID. No se puede añadir el libro.");
-      } else {
+      
         // Crea un nuevo libro
         let modBook: Books = new Books(title, type, author, Number(price), photo, Number(id_book));
         // Llama al método add del servicio BooksService que añade el libro al array de libros
         this.booksService.edit(modBook);
-      }
+      
     } else {
       alert("Por favor, rellene todos los campos");
     }
