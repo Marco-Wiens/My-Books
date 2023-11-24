a69db77e87863aacc7e1b1c4d96e1bdbdb2c14bc
Marco-Wiens <marco.wiens@gmail.com>
2023-11-24 14:06:48 +0100

    apiRest

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { Books } from 'src/app/models/books';
+import { Respuesta } from 'src/app/models/respuesta';
 import { FormatoLibrosPipe } from 'src/app/pipes/formato-libros.pipe';
 import { BooksService } from 'src/app/shared/books.service';
 
@@ -15,32 +17,62 @@ export class BooksComponent implements OnInit{
   
 
   
-  books: Books[];
+  // books: Books[];
   searchItem: string = "";
 
-  constructor(public booksService:BooksService){
-    this.books = this.booksService.getAll();
+  constructor(public booksService:BooksService, public toastr: ToastrService){
+    
   }
 
   
 
 
   ngOnInit(): void {
-
+    this.mostrarTodos();
   }
 
-  deleteBook(i:number){
+  public mostrarTodos(){
+    this.booksService.getAll().subscribe((respuesta: Respuesta) => {
+      if (respuesta.error) {
+        this.toastr.error(respuesta.mensaje);
+      } else {
+        const dataAsArray: Books[] = Array.isArray(respuesta.data) ? respuesta.data : [respuesta.data];
+        this.booksService.books = dataAsArray;
+      }
+    });
+  }
+
+  deleteBook(id_libro: number){
     
-    this.booksService.delete(i);
-    // Recargamos el array sin el libro borrado
-    this.books = this.booksService.getAll();
+    this.booksService.delete(id_libro).subscribe((respuesta: Respuesta) => {
+      if (respuesta.error) {
+        this.toastr.error(respuesta.mensaje);
+      } else {
+        this.toastr.success(respuesta.mensaje);
+        const dataAsArray: Books[] = Array.isArray(respuesta.data) ? respuesta.data : [respuesta.data];
+        this.booksService.books = dataAsArray;
+      }
+    });
   }
 
 
-  buscar(){
+  buscar(id_libro: HTMLInputElement){
     // Llama a la función getOne con el término de búsqueda actual
 
-    this.books = this.booksService.getOne(this.searchItem);
+    this.booksService.getOne(id_libro.value).subscribe((respuesta: Respuesta) => {
+      console.log(id_libro.value);
+      
+        if (id_libro.value) {
+          if (respuesta.error) {
+            this.toastr.error(respuesta.mensaje);
+          } else {
+            const dataAsArray: Books[] = Array.isArray(respuesta.data) ? respuesta.data : [respuesta.data];
+            this.booksService.books = dataAsArray;
+          }
+        } else {
+          this.mostrarTodos();
+        }
+      });
     
     
   }
